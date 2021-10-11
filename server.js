09ac137c64c1033156a4bd72a8ce5a11b0898ccb
Jiya Chauhan <76962214+jiya76@users.noreply.github.com>
2021-10-11 22:06:51 +0530

    removing files

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app= express()
-const port= 4000
-app.set('view engine','html')
-app.use(express.static('home page'))
-app.get('/',(req,res)=> {res.render('signlog.html');})
-// app.get('/',(req,res)=> {res.render('index.html')}
-app.listen(port,()=>{console.log(`server running ${port}`);})
